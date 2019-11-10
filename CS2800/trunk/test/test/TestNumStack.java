683d9a379943a7634ea6d5c4026ca1e8c66b3690
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-11-10 21:30:14 +0000

    Created NumStack Class and TestNumStack with a push and isEmpty Method for floats.

---
+++ CS2800/trunk/test/test/TestNumStack.java
@@ -0,0 +1,27 @@
+package test;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import stack.NumStack;
+
+
+/** TDD for TestNumStack.java
+ * @author zfac237
+ *
+ */
+public class TestNumStack {
+
+  /** Creates a Stack object and checks if after pushing an entry the stack contains and item.
+   * The test allows for the creation of a push() function which loads up a stack.
+   */
+  @Test
+  public void pushTest() { // TEST 29 - Pushing a float test.
+    NumStack test = new NumStack();
+    test.push(56.25f);
+    assertFalse("isEmpty() method did not return false for a non-empty stack", test.isEmpty());
+  }
+
+}
+
