2c1b83b3499962c5e3b43e750a9407f6608d2982
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-11-10 22:20:58 +0000

    Created TestNumStack and NumStack classes and added push and isEmpty methods for float values.

--- CS2800/branches/NumStackBranch/src/stack/NumStack.java
+++ CS2800/branches/NumStackBranch/src/stack/NumStack.java
@@ -1,5 +1,20 @@
 package stack;
 
 public class NumStack {
+  private int size = 0;
+  Stack numStack = new Stack();
+
+  public NumStack() {
+    this.size = 0;
+  }
+
+  public void push(float f) {
+    size += 1;
+    numStack.push(new Entry(f));
+  }
+
+  public boolean isEmpty() {
+    return size == 0;
+  }
 
 }
