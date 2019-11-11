e17c079a5a13ace9a35928dec5bb834512c7c4c8
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-11-11 14:13:45 +0000

    Fixed NumStack class to input and output float values correctly, also fixed the isEmpty method to use the original stack isEmpty method

--- CS2800/branches/NumStackBranch/src/stack/NumStack.java
+++ CS2800/branches/NumStackBranch/src/stack/NumStack.java
@@ -1,8 +1,5 @@
 package stack;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class NumStack {
   private int size = 0;
   Stack numStack = new Stack();
@@ -12,20 +9,15 @@ public class NumStack {
   }
 
   public void push(float f) {
-    size += 1;
     numStack.push(new Entry(f));
   }
 
   public boolean isEmpty() {
-    return size == 0;
+    return numStack.isEmpty();
   }
 
-  public Entry pop() {
-    if (size <= 0) {
-      throw new EmptyStackException("Stack is empty");
-    } else {
-      Entry last = Stack.entries.remove(--size);
-      return last;
-    } 
+  public float pop() {
+    Entry floatval = numStack.pop();
+    return floatval.getFloat();
   }
 }
