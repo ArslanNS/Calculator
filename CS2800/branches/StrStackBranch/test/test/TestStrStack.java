c0c8acba3e30c19035ccf7294c2f172149656ce9
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-11-16 15:53:54 +0000

    TestStrStack class with the tests to create the StrStack class moved from trunk to branched and added StrStack class

---
+++ CS2800/branches/StrStackBranch/test/test/TestStrStack.java
@@ -0,0 +1,39 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import stack.StrStack;
+
+import stack.Symbol;
+
+/**TDD for TestNumStack.java
+ * @author zfac237
+ *
+ */
+public class TestStrStack {
+
+  /**Creates a Stack object and checks if after pushing an entry the stack contains and item.
+   * The test allows for the creation of a push() function which loads up a stack.
+   */
+  @Test
+  public void strStackPushTest() { // TEST 33 - Pushing a symbol test.
+    StrStack test = new StrStack();
+    test.push("hello world");
+    assertFalse("isEmpty() method did not return false for a non-empty stack", test.isEmpty());
+  }
+
+  /** Creates a Stack object and checks if the Stack items can be removed after being pushed.
+   * The test uses isEmpty to check whether pushing then popping leaves the stack empty.
+   */
+  @Test
+  public void stackStackPopTest() { // TEST 34 - Removing a value test.
+    StrStack test = new StrStack();
+    test.push("hello world");
+    String stringPop = test.pop();
+    assertEquals("hello world not popped' ","hello world", String.valueOf(stringPop.toString()));
+  }
+}
