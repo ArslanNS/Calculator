82a3951f0e5383c20026da04c3f7e0aa2eccd426
zfac237 <zfac237@8f148db6-ad3b-4b66-84c6-d962692bf993>
2019-12-09 14:59:08 +0000

    Moving files and clearing up SVN

--- CS2800/branches/gui/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"stack","l":"BadType"},{"p":"stack","l":"EmptyStackException"},{"p":"stack","l":"Entry"},{"p":"stack","l":"Stack"},{"p":"stack","l":"Symbol"},{"p":"test","l":"TestEntry"},{"p":"test","l":"TestStack"},{"p":"test","l":"TestSymbol"},{"p":"test","l":"TestType"},{"p":"stack","l":"Type"}]
