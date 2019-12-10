3e84c98d4a67bcfdccedd7955d428ca75d28930f
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-12-10 19:09:24 +0000

    Finished CalculatorFactory for all operators along with the TDD tests.

--- CS2800/branches/CalculatorFactoryBranch/src/calculator/CalculatorFactory.java
+++ CS2800/branches/CalculatorFactoryBranch/src/calculator/CalculatorFactory.java
@@ -1,21 +1,33 @@
 package calculator;
 
 import operation.Addition;
+import operation.Divide;
+import operation.Multiply;
+import operation.Subtract;
 import stack.NumStack;
 
-public class CalculatorFactory {
+public class CalculatorFactory { 
   
   /** class that operates on two values.
    * @param stack that has values pushed on it.
    * @param str the operator that decides what to do with the values.
    * @return float that is the two values which have been operated on
    */
-  public float calculatorEquations(NumStack stack, String str) {
+  public float calculatorEquations(NumStack stack, String str) throws OperatorException {
     if (str == "+") {
       Addition add = new Addition();
       return add.evaluate(stack.pop(), stack.pop());
+    } else if (str == "-") {
+      Subtract sub = new Subtract();
+      return sub.evaluate(stack.pop(), stack.pop());
+    } else if (str == "*") {
+      Multiply times = new Multiply();
+      return times.evaluate(stack.pop(), stack.pop());
+    } else if (str == "/") {
+      Divide divide = new Divide();
+      return divide.evaluate(stack.pop(), stack.pop());
     } else {
-      return 0;
+      throw new OperatorException("Operator not recognised");
     }
   } 
 }
