82a8c5c189ba42d77a939180551127780aacc093
zfac237 <zfac237@8f148db6-ad3b-4b66-84c6-d962692bf993>
2019-12-10 21:14:49 +0000

    TDD tests to check if the code works with other operators and multiple operators.

--- CS2800/branches/RevPolCalcBranch/test/test/TestRevPolCalc.java
+++ CS2800/branches/RevPolCalcBranch/test/test/TestRevPolCalc.java
@@ -19,4 +19,44 @@ public class TestRevPolCalc {
     float val = revPolCalc.evaluate("5 3 +");
     assertEquals("the value returned was not correct", 8f, val, 0f);
   }
+  
+  /** Creates several objects and passes them to a RevPolCalc object. 
+   *  The test checks the evaluate subtracts correctly. 
+   */
+  @Test
+  public void testFactorySubtract() { //TEST 44 - test to subtract two values.
+    RevPolCalc revPolCalc = new RevPolCalc();
+    float val = revPolCalc.evaluate("5 3 -");
+    assertEquals("the value returned was not correct", 2f, val, 0f);
+  }
+  
+  /** Creates several objects and passes them to a RevPolCalc object. 
+   *  The test checks the evaluate multiplies correctly. 
+   */
+  @Test
+  public void testFactoryMulitply() { //TEST 45 - test to multiply two values.
+    RevPolCalc revPolCalc = new RevPolCalc();
+    float val = revPolCalc.evaluate("5 3 *");
+    assertEquals("the value returned was not correct", 15f, val, 0f);
+  }
+  
+  /** Creates several objects and passes them to a RevPolCalc object. 
+   *  The test checks the evaluate divide correctly. 
+   */
+  @Test
+  public void testFactoryDivide() { //TEST 46 - test to divide two values.
+    RevPolCalc revPolCalc = new RevPolCalc();
+    float val = revPolCalc.evaluate("15 3 /");
+    assertEquals("the value returned was not correct", 5f, val, 0f);
+  }
+  
+  /** Creates several objects and passes them to a RevPolCalc object. 
+   *  The test checks the evaluate works with multiple operators. 
+   */
+  @Test
+  public void testFactoryMultiple() { //TEST 47 - test with multiple operators.
+    RevPolCalc revPolCalc = new RevPolCalc();
+    float val = revPolCalc.evaluate("5 3 + 2 / 2 * 5 -");
+    assertEquals("the value returned was not correct", 3f, val, 0f);
+  }
 }
