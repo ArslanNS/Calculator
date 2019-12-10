d34f0fa62953613f6370b4b3ac46b6fe88eca9dc
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-12-10 17:54:53 +0000

    Branch to work on calculatorFactory.

---
+++ CS2800/branches/CalculatorFactoryBranch/src/operation/Subtract.java
@@ -0,0 +1,17 @@
+package operation;
+
+/**Class to subtract float values.
+ * @author zfac237
+ *
+ */
+public class Subtract implements Calculator {
+  
+  /** evaluate method to subtract two float values.
+   * @paramf1 
+   * @paramf2
+   * @return float that is f1 subtracted by f2.
+   */
+  public float evaluate(float f1, float f2) {
+    return f1 - f2;
+  }
+}
