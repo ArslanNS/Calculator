9b5d844ac5ffe4b5f2fd4916adf849fc1d6fa33b
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-12-11 22:46:25 +0000

    TDD tests for the rest of the Symbol selects.

--- CS2800/branches/SymbolBranch/test/test/TestStandardSymbols.java
+++ CS2800/branches/SymbolBranch/test/test/TestStandardSymbols.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 import stack.Symbol;
+import standardcalc.StandardSymbols;
 
 /** TDD for symbols for the standard calculator.
  * @author zfac237
@@ -15,10 +16,60 @@ public class TestStandardSymbols {
    * Test returns the correct symbol when symbol is selected.
    */
   @Test
-  public void addSymbolTest() { //TEST 48  
+  public void addSymbolTest() { //TEST 48  - test to see if plus is returned as symbol.
     StandardSymbols add = new StandardSymbols();
     Symbol sym = add.selector("+");
-    assertEquals("symbol returned was not '+'", "+", sym);
+    assertEquals("symbol returned was not '+'", Symbol.PLUS, sym);
+  }
+  
+  /** Test to check if selector method works.
+   * Test returns the correct symbol when symbol is selected.
+   */
+  @Test
+  public void minusSymbolTest() { //TEST 49 - test to see if plus is returned as symbol.
+    StandardSymbols add = new StandardSymbols();
+    Symbol sym = add.selector("+");
+    assertEquals("symbol returned was not '+'", Symbol.PLUS, sym);
+  }
+  
+  /** Test to check if selector method works.
+   * Test returns the correct symbol when symbol is selected.
+   */
+  @Test
+  public void divideSymbolTest() { //TEST 50 - test to see if plus is returned as symbol.
+    StandardSymbols add = new StandardSymbols();
+    Symbol sym = add.selector("+");
+    assertEquals("symbol returned was not '+'", Symbol.PLUS, sym);
+  }
+  
+  /** Test to check if selector method works.
+   * Test returns the correct symbol when symbol is selected.
+   */
+  @Test
+  public void timeSymbolTest() { //TEST 51 - test to see if plus is returned as symbol.
+    StandardSymbols add = new StandardSymbols();
+    Symbol sym = add.selector("+");
+    assertEquals("symbol returned was not '+'", Symbol.PLUS, sym);
+  }
+  
+  /** Test to check if selector method works.
+   * Test returns the correct symbol when symbol is selected.
+   */
+  @Test
+  public void riBracketSymbolTest() { //TEST 52 - test to see if plus is returned as symbol.
+    StandardSymbols add = new StandardSymbols();
+    Symbol sym = add.selector("+");
+    assertEquals("symbol returned was not '+'", Symbol.PLUS, sym);
+  }
+  
+  /** Test to check if selector method works.
+   * Test returns the correct symbol when symbol is selected.
+   */
+  @Test
+  public void leBracketSymbolTest() { //TEST 53 - test to see if plus is returned as symbol. 
+    StandardSymbols add = new StandardSymbols();
+    Symbol sym = add.selector("+");
+    assertEquals("symbol returned was not '+'", Symbol.PLUS, sym);
   }
 
 }
