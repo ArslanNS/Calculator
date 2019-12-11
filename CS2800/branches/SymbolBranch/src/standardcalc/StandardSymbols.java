19219310ed1e4cff27d88a0aa3fdc8f3cfbbf9cd
zfac237 <zfac237@8f148db6-ad3b-4b66-84c6-d962692bf993>
2019-12-11 22:46:25 +0000

    TDD tests for the rest of the Symbol selects.

---
+++ CS2800/branches/SymbolBranch/src/standardcalc/StandardSymbols.java
@@ -0,0 +1,29 @@
+package standardcalc;
+
+import stack.Symbol;
+
+public class StandardSymbols {
+
+  /** Method to select correct symbol from string input.
+   * @param str the symbol that needs to be converted to Symbol form.
+   * @return sym symbol that is being selected.
+   */
+  public Symbol selector(String str) {
+    Symbol sym = null;
+    if (str.equals(Symbol.PLUS.toString())) {
+      sym = Symbol.PLUS;
+    } else if (str.equals(Symbol.MINUS.toString())) {
+      sym = Symbol.MINUS;
+    } else if (str.equals(Symbol.DIVIDE.toString())) {
+      sym = Symbol.DIVIDE;
+    } else if (str.equals(Symbol.TIMES.toString())) {
+      sym = Symbol.TIMES;
+    } else if (str.equals(Symbol.LEFT_BRACKET.toString())) {
+      sym = Symbol.LEFT_BRACKET;
+    } else if (str.equals(Symbol.RIGHT_BRACKET.toString())) {
+      sym = Symbol.RIGHT_BRACKET;
+    } 
+    return sym;
+  }
+
+}
