4d0d3e92a57c95b9ec073e48adf3891cf3fbdeca
zfac237 <44402229+ArslanNS@users.noreply.github.com>
2019-12-12 12:38:06 +0000

    branch to work on standard calculator.

---
+++ CS2800/branches/StandardCalculatorBranch/src/operation/Divide.java
@@ -0,0 +1,17 @@
+package operation;
+
+/**Class to divide float values.
+ * @author zfac237
+ *
+ */
+public class Divide implements Calculator {
+  
+  /** evaluate method to divide two float values.
+   * @paramf1 
+   * @paramf2
+   * @return float that is f1 divided by f2.
+   */
+  public float evaluate(float f1, float f2) {
+    return f2 / f1;
+  }
+}
