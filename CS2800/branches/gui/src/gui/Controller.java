963b14260b248550ebb3c3f018d024c416b76ff0
zfac237 <zfac237@8f148db6-ad3b-4b66-84c6-d962692bf993>
2019-12-13 01:12:30 +0000

    Javafx Gui View for calculator.

--- CS2800/branches/gui/src/gui/Controller.java
+++ CS2800/branches/gui/src/gui/Controller.java
@@ -1,4 +1,24 @@
 package gui;
 
+import calculator.CalcModel;
+
 public class Controller {
+  ViewInterface myView = null;
+  CalcModel calc = new CalcModel();
+  
+  private void calculateAction() {
+    float answer = calc.evaluate(myView.getExpression(), myView.isInfix());
+    myView.setAnswer(Float.toString(answer));
+  }
+
+  private void changeType(OpType type) {
+    myView.setAnswer("Changed to " + type);
+  }
+
+  public Controller(ViewInterface view) {
+    myView = view;
+    view.calcObserver(this::calculateAction);
+    view.typeObserver(this::changeType);
+  }
+
 }
